3a76c3e4ba469fda8423297e63127670ebc4cf92
Rob Napier <robnapier@gmail.com>
2014-09-10 10:00:58 -0400

    Try using dispatch_group for the completion handlers, too.

--- LlamaKit/Future.swift
+++ LlamaKit/Future.swift
@@ -9,8 +9,7 @@ import Foundation
 private let sharedProcessingQueue = dispatch_queue_create("llama.future.shared-processing", DISPATCH_QUEUE_CONCURRENT)
 
 public class Future<T> {
-  private var _value: T?
-  var onCompleteHandlers: [(T -> ())] = []
+  private var _value: Result<T>?
 
   // The resultQueue is used to read the result. It begins suspended
   // and is resumed once a result exists.
@@ -26,7 +25,7 @@ public class Future<T> {
     dispatch_group_enter(self.resultReadyGroup)
   }
 
-  public convenience init(_ f: () -> T, queue: dispatch_queue_t = sharedProcessingQueue) {
+  public convenience init(_ f: () -> Result<T>, queue: dispatch_queue_t = sharedProcessingQueue) {
     self.init(queue: queue)
     dispatch_async(self.processingQueue) { self.completeWith(f()) }
   }
@@ -39,26 +38,46 @@ public class Future<T> {
     return isCompleted
   }
 
-  public func onComplete(f: T -> ()) {
-    dispatch_async(mutateQueue) {
-      self.onCompleteHandlers += [f]
+  // FIXME: Use dispatch_group_notify to schedule
+  public func onComplete(f: Result<T> -> ()) {
+    dispatch_group_notify(self.resultReadyGroup, processingQueue) { f(self._value!) }
+  }
+
+  // FIXME: Implement on top of onComplete w/ switch
+  public func onSuccess(f: T -> ()) {
+    dispatch_group_notify(self.resultReadyGroup, self.processingQueue) {
+      switch self._value! {
+      case .Success(let box):
+        dispatch_group_notify(self.resultReadyGroup, self.processingQueue) { f(box.unbox) }
+      case .Failure(_): return
+      }
     }
   }
 
-  internal func completeWith(x: T) {
+  // FIXME: Implement on top of onComplete w/ switch
+  public func onFailure(f: NSError -> ()) {
+    dispatch_group_notify(self.resultReadyGroup, self.processingQueue) {
+      switch self._value! {
+      case .Success(_): return
+      case .Failure(let err): dispatch_group_notify(self.resultReadyGroup, self.processingQueue) { f(err) }
+      }
+    }
+  }
+
+  internal func completeWith(x: Result<T>) {
     dispatch_async(mutateQueue) {
       precondition(self._value == nil, "Future cannot complete more than once")
       self._value = x
-      for handler in self.onCompleteHandlers { handler(x) }
+//      for handler in self.onCompleteHandlers { handler(success(x)) }
       dispatch_group_leave(self.resultReadyGroup)
     }
   }
 
-  public func result() -> T {
+  public func result() -> Result<T> {
     return self.waitResult()!
   }
 
-  public func waitResult(timeout: dispatch_time_t = DISPATCH_TIME_FOREVER) -> T? {
+  public func waitResult(timeout: dispatch_time_t = DISPATCH_TIME_FOREVER) -> Result<T>? {
     if dispatch_group_wait(self.resultReadyGroup, timeout) == 0 {
       return self._value!
     } else {
@@ -67,18 +86,39 @@ public class Future<T> {
   }
 
   public func map<U>(f: T -> U) -> Future<U> {
-    return future { f(self.result()) }
+    let newFuture = Future<U>()
+    self.onComplete { r in
+      switch r {
+      case .Success(let box): newFuture.completeWith(success(f(box.unbox)))
+      case .Failure(let err): newFuture.completeWith(failure(err))
+      }
+    }
+    return newFuture
   }
 
   public func flatMap<U>(f: T -> Future<U>) -> Future<U> {
-    return future { f(self.result()).result() }
+    let newFuture = Future<U>()
+    self.onComplete { r in
+      switch r {
+      case .Success(let box): newFuture.completeWith(f(box.unbox).result())
+      case .Failure(let err): newFuture.completeWith(failure(err))
+      }
+    }
+    return newFuture
   }
 }
 
 func sequence<T>(futures: [Future<T>]) -> Future<[T]> {
   return future {
-    futures.reduce([]) { acc, f in
-      return acc + [f.result()]
+    return futures.reduce(success([T]())) { acc, fu in
+      switch acc {
+      case .Success(let accBox):
+        switch fu.result() {
+        case .Success(let resultBox): return success(accBox.unbox + [resultBox.unbox])
+        case .Failure(let err): return failure(err)
+        }
+      case .Failure(let err): return acc
+      }
     }
   }
 }
@@ -90,6 +130,6 @@ func <**><T,U>(x: Future<T>, f: T -> U) -> Future<U> {
   return x.map(f)
 }
 
-func future<T>(f: () -> T) -> Future<T> {
+func future<T>(f: () -> Result<T>) -> Future<T> {
   return Future(f)
 }
