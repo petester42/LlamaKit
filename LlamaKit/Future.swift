ea5e817d9fa4adf2b09f4cd09c180c93cd163492
Rob Napier <robnapier@gmail.com>
2014-09-11 15:59:55 -0400

    Remove Result from Future. A Future returns a T. A Task (? probably ?) returns a Result<T>

--- LlamaKit/Future.swift
+++ LlamaKit/Future.swift
@@ -6,21 +6,7 @@
 
 import Foundation
 
-//
-// IN PROGRESS: Will probably split out Future (returning T) from Task (returning Result<T>).
-//
-
 private let sharedFutureProcessingQueue = dispatch_queue_create("llama.future.shared-processing", DISPATCH_QUEUE_CONCURRENT)
-
-//var future: Future = Future.withQueue(sharedFutureProcessingQueue)
-//func withQueue(queue: dispatch_queue_t) -> <T>(f: () -> Result<T>) -> Future<T> {
-//  return Future<T>(queue: queue, f)
-//}
-
-public func FutureWithQueue<T>(queue: dispatch_queue_t)(f: () -> Result<T>) -> Future<T> {
-  return Future<T>(queue: queue, f)
-}
-
 
 public class Future<T> {
 
@@ -28,14 +14,14 @@ public class Future<T> {
   let mutateQueue = dispatch_queue_create("llama.future.value", DISPATCH_QUEUE_SERIAL)
   let processingQueue: dispatch_queue_t
 
-  private var _value: Result<T>?
+  private var _value: T?
 
   internal init(queue: dispatch_queue_t) {
     self.processingQueue = queue
     dispatch_group_enter(self.resultReadyGroup)
   }
 
-  public convenience init(queue: dispatch_queue_t, _ f: () -> Result<T>) {
+  public convenience init(queue: dispatch_queue_t, _ f: () -> T) {
     self.init(queue: queue)
     dispatch_async(self.processingQueue) { self.completeWith(f()) }
   }
@@ -49,30 +35,11 @@ public class Future<T> {
     return isCompleted
   }
 
-  public func onComplete(f: Result<T> -> ()) {
+  public func onComplete(f: T -> ()) {
     dispatch_group_notify(self.resultReadyGroup, processingQueue) { f(self._value!) }
   }
 
-  public func onSuccess(f: T -> ()) {
-    dispatch_group_notify(self.resultReadyGroup, self.processingQueue) {
-      switch self._value! {
-      case .Success(let box):
-        dispatch_group_notify(self.resultReadyGroup, self.processingQueue) { f(box.unbox) }
-      case .Failure(_): return
-      }
-    }
-  }
-
-  public func onFailure(f: NSError -> ()) {
-    dispatch_group_notify(self.resultReadyGroup, self.processingQueue) {
-      switch self._value! {
-      case .Success(_): return
-      case .Failure(let err): dispatch_group_notify(self.resultReadyGroup, self.processingQueue) { f(err) }
-      }
-    }
-  }
-
-  internal func completeWith(x: Result<T>) {
+  internal func completeWith(x: T) {
     dispatch_async(mutateQueue) {
       precondition(self._value == nil, "Future cannot complete more than once")
       self._value = x
@@ -80,11 +47,11 @@ public class Future<T> {
     }
   }
 
-  public func result() -> Result<T> {
+  public func result() -> T {
     return self.waitResult()!
   }
 
-  public func waitResult(timeout: dispatch_time_t = DISPATCH_TIME_FOREVER) -> Result<T>? {
+  public func waitResult(timeout: dispatch_time_t = DISPATCH_TIME_FOREVER) -> T? {
     if dispatch_group_wait(self.resultReadyGroup, timeout) == 0 {
       return self._value!
     } else {
@@ -94,39 +61,20 @@ public class Future<T> {
 
   public func map<U>(f: T -> U) -> Future<U> {
     let newFuture = Future<U>(queue: self.processingQueue)
-    self.onComplete { r in
-      switch r {
-      case .Success(let box): newFuture.completeWith(success(f(box.unbox)))
-      case .Failure(let err): newFuture.completeWith(failure(err))
-      }
-    }
+    self.onComplete { x in newFuture.completeWith(f(x)) }
     return newFuture
   }
 
   public func flatMap<U>(f: T -> Future<U>) -> Future<U> {
     let newFuture = Future<U>(queue: self.processingQueue)
-    self.onComplete { r in
-      switch r {
-      case .Success(let box): newFuture.completeWith(f(box.unbox).result())
-      case .Failure(let err): newFuture.completeWith(failure(err))
-      }
-    }
+    self.onComplete { x in newFuture.completeWith(f(x).result()) }
     return newFuture
   }
 }
 
 public func sequence<T>(futures: [Future<T>]) -> Future<[T]> {
   return future {
-    return futures.reduce(success([T]())) { acc, fu in
-      switch acc {
-      case .Success(let accBox):
-        switch fu.result() {
-        case .Success(let resultBox): return success(accBox.unbox + [resultBox.unbox])
-        case .Failure(let err): return failure(err)
-        }
-      case .Failure(let err): return acc
-      }
-    }
+    return futures.reduce([T]()) { acc, x in acc + [x.result()] }
   }
 }
 
@@ -137,6 +85,6 @@ public func <**><T,U>(x: Future<T>, f: T -> U) -> Future<U> {
   return x.map(f)
 }
 
-public func future<T>(f: () -> Result<T>) -> Future<T> {
+public func future<T>(f: () -> T) -> Future<T> {
   return Future(queue: sharedFutureProcessingQueue, f)
 }
---
+++ LlamaKit/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  LlamaKit
+//
+//  Created by Rob Napier on 9/11/14.
+//  Copyright (c) 2014 Rob Napier. All rights reserved.
+//
+
+import Foundation
