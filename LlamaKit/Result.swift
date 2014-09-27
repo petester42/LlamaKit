7485536185461cc2b7b3c39cfd64ca4d5dc870fd
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-09-26 22:05:17 -0700

    Fix visibility on Box members

--- LlamaKit/Result.swift
+++ LlamaKit/Result.swift
@@ -121,6 +121,6 @@ public func ??<T>(result: Result<T>, defaultValue: @autoclosure () -> T) -> T {
 /// Due to current swift limitations, we have to include this Box in Result.
 /// Swift cannot handle an enum with multiple associated data (A, NSError) where one is of unknown size (A)
 final public class Box<T> {
-  let unbox: T
-  init(_ value: T) { self.unbox = value }
+  public let unbox: T
+  public init(_ value: T) { self.unbox = value }
 }
