203caa2de105e1462ab8c791318ca005e12629a3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-09-26 21:56:22 -0700

    Add OS X target, import framework module in tests

--- LlamaKit/LlamaKit.h
+++ LlamaKit/LlamaKit.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Rob Napier. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for LlamaKit.
 FOUNDATION_EXPORT double LlamaKitVersionNumber;
--- LlamaKitTests/ResultTests.swift
+++ LlamaKitTests/ResultTests.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import LlamaKit
 import XCTest
 
 class ResultTests: XCTestCase {
